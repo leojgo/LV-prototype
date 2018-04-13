4bbc9bd971cdcbe9e13d96a606717893fb048f97
ushapax <usha@paxrodentia.com>
2018-04-12 20:03:57 -0500

    oops — make sure dev builds have console logging enabled

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './routes.js';
 
-console = {};
-console.log = function() {} 
+//console = {};
+//console.log = function() {} 
 //silence logging for now
 
 //import UIkit from './js/uikit'; 
